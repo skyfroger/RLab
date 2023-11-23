7623bd02823946a1dc1f6b72553ca32e12201ddd
skyfroger <skyfroger@gmail.com>
2023-11-23 23:03:35 +0300

    init

---
+++ src/RLab.cpp
@@ -0,0 +1,9 @@
+#include "Arduino.h"
+#include "RLab.h"
+
+/**
+ * @brief Конструктор класса RPulse
+ */
+RPulse::RPulse()
+{
+}
---
+++ src/RLab.h
@@ -0,0 +1,14 @@
+#ifndef RLab_h
+#define RLab_h
+
+#include "Arduino.h"
+
+class RLab
+{
+public:
+    RLab(); // конструктор
+
+private:
+};
+
+#endif
