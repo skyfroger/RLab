545b6f9e307bf05f0b1fe197eca035707ba36964
skyfroger <skyfroger@gmail.com>
2023-11-24 01:15:45 +0300

    feat: методы работы с номерными LED

--- src/RLab.cpp
+++ src/RLab.cpp
@@ -4,6 +4,68 @@
 /**
  * @brief Конструктор класса RPulse
  */
-RPulse::RPulse()
+RLab::RLab()
 {
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(YELLOW_LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
+
+    pinMode(DATA, OUTPUT);
+    pinMode(CLK, OUTPUT);
+    pinMode(RESET, OUTPUT);
+
+    // контакт сброса у регистра инвертирован,
+    // значит нужно подавать 1, чтобы регистр
+    // постоянно не обнулялся
+    digitalWrite(RESET, HIGH);
+
+    // изначально все номерные светодиоды выключены
+    this->ledState = 0b00000000;
+}
+
+/**
+ * @brief Включение номерного светодиода
+ *
+ * @param pos номер светодиода
+ */
+void RLab::ledOn(int pos)
+{
+    if (pos >= 0 && pos <= 8)
+    {
+        bitSet(this->ledState, pos);
+        this->updateShiftReg();
+    }
+}
+
+/**
+ * @brief Выключение номерного светодиода
+ *
+ * @param pos номер светодиода
+ */
+void RLab::ledOff(int pos)
+{
+    if (pos >= 0 && pos <= 8)
+    {
+        bitClear(this->ledState, pos);
+        this->updateShiftReg();
+    }
+}
+
+/**
+ * @brief Переключение состояния номерного светодиода
+ *
+ * @param pos номер светодиода
+ */
+void RLab::ledToggle(int pos)
+{
+    if (pos >= 0 && pos <= 8)
+    {
+        this->ledState ^= 1 << pos;
+        this->updateShiftReg();
+    }
+}
+
+void RLab::updateShiftReg()
+{
+    shiftOut(DATA, CLK, MSBFIRST, this->ledState);
 }
--- src/RLab.h
+++ src/RLab.h
@@ -3,12 +3,34 @@
 
 #include "Arduino.h"
 
+// пины для кнопок
+#define BUTTON1 8
+#define BUTTON2 9
+#define BUTTON3 10
+#define BUTTON4 12
+#define BUTTON5 11
+
+// пины для разноцветных светодиодов
+#define GREEN_LED 5
+#define YELLOW_LED 6
+#define RED_LED 7
+
+// пины сдвигового регистра
+#define DATA 2   // пин данных
+#define CLK 4    // пин синхронизации
+#define RESET 19 // пин сброса
+
 class RLab
 {
 public:
-    RLab(); // конструктор
+    RLab();                  // конструктор
+    void ledOn(int pos);     // включение светодиода под номером pos
+    void ledOff(int pos);    // выключение светодиода под номером pos
+    void ledToggle(int pos); // переключение светодиода
 
 private:
+    byte ledState;         // значения на выходе сдвигового регистра
+    void updateShiftReg(); // обновление состояния регистра
 };
 
 #endif
